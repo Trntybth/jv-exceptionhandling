de318011a1ce66ad491c608840dfddf73691ac9a
Trntybth <trinitybooth89@gmail.com>
2024-07-31 11:10:35 +0100

    feat: Added try/catch and throw to divide method

--- src/main/java/exercises/Exercise1.java
+++ src/main/java/exercises/Exercise1.java
@@ -3,10 +3,13 @@ package exercises;
 public class Exercise1 {
 
     public static int divide(int numerator, int denominator) {
-
-
-
-
+        int division = 0;
+        try {
+            division = numerator / denominator;
+        } catch (ArithmeticException e ){
+           throw e;
+        }
+        return division;
     }
 
 }
