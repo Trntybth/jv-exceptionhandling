fdb95a0ebcf701d2056c7e85beac25e3e0f7c3a2
Trntybth <trinitybooth89@gmail.com>
2024-07-31 11:01:54 +0100

    feat: Created class, added to main, created empty method

---
+++ src/main/java/exercises/Exercise1.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class Exercise1 {
+
+    public static int divide(int numerator, int denominator) {
+
+
+
+
+    }
+
+}
---
+++ src/main/java/exercises/Main.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Exercise1.divide(10, 2));
+        System.out.println(Exercise1.divide(10, 0));
+    }
+
+}
