ad78ac0c9dc5cc48cb4fce6cbdc9358cc7274b5c
Liam O'Hara <liam.ohara@gmail.com>
2024-07-31 11:56:26 +0100

    Feat: implement getUserInput method with try/catch within while loop.

--- src/main/java/exercises/Exercise2.java
+++ src/main/java/exercises/Exercise2.java
@@ -6,10 +6,28 @@ import java.util.Scanner;
 public class Exercise2 {
 
     public static int[] getUserInput() {
+        int[] integers = new int[2];
+        Scanner intInput = new Scanner(System.in);
 
-        while (true) {
+
+
+       while (true) {
+        try {
+            System.out.println("Enter divisor:");
+            integers[0] = intInput.nextInt();
+            System.out.println("Enter dividend:");
+            integers[1] = intInput.nextInt();
+        }
+        catch (InputMismatchException e) {
+            System.err.println("Invalid input. Please try again.");
+            getUserInput();
+            continue;
 
         }
+        return integers;
+
+       }
+
 
     }
 
